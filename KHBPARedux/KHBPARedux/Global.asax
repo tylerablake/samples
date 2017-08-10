e5d354a2d76f4d540180e2ef9919ca53cb4ab115
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-08-10 18:46:54 -0400

    Added khbpa redux app

---
+++ KHBPARedux/KHBPARedux/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KHBPARedux.MvcApplication" Language="C#" %>
---
+++ KHBPARedux/KHBPARedux/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KHBPARedux.Startup))]
+namespace KHBPARedux
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
