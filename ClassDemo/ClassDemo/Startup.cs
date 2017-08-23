561f70051ff8a4b9b16616ce88d4a5711a77fdd4
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-08-23 16:31:24 -0400

    Added ClassDemo for Student CRUD

---
+++ ClassDemo/ClassDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassDemo.Startup))]
+namespace ClassDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
