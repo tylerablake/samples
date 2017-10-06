967144005e6536c23cf96b41cfea93556dda0593
unknown <tyler.blake.louisville@gmail.com>
2017-10-06 16:18:36 -0400

    Remove developer university

--- DeveloperUniversity/DeveloperUniversity/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeveloperUniversity.MvcApplication" Language="C#" %>
--- DeveloperUniversity/DeveloperUniversity/Models/ViewModels/MailViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeveloperUniversity.Models.ViewModels
-{
-    public class MailViewModel
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Messge { get; set; }
-    }
-}
