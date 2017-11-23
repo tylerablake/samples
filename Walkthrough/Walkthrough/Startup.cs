349268b426190f7eeb54f271fd58187f058db388
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-11-23 12:14:11 -0500

    Initial commit for .net walkthrough project

---
+++ Walkthrough/Walkthrough/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Walkthrough.Startup))]
+namespace Walkthrough
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
